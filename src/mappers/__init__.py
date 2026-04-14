616aacae95929c08746a9319e3efdc21588ff006
Andrew <andrew@greatstrength.me>
2026-04-13 20:51:22 -0700

    Mappers - Fix typo in DeclarationAggregate alias and format imports in __init__.py

--- src/mappers/__init__.py
+++ src/mappers/__init__.py
@@ -4,4 +4,15 @@
 
 # ** app
 from .lexer import TokenAggregate
-from .ast import DeclarationAggregate, DeclarationAggregate as DeclAggragate
+from .ast import (
+    DeclarationAggregate, 
+    DeclarationAggregate as Decl,
+    ExpressionAggregate,
+    ExpressionAggregate as Expr,
+    StatementAggregate,
+    StatementAggregate as Stmt,
+    TypeAggregate,
+    TypeAggregate as Type,
+    ParamListAggregate,
+    ParamListAggregate as ParamList,
+    )
